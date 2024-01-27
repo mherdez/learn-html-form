854f5bef425085aae3e51fc21cad4c3e30e1ecf0
Manuel Herdez <herdez@gmail.com>
2024-01-27 14:27:27 -0600

    Creando tarjeta de usuario a partir del formulario

--- app.js
+++ app.js
@@ -8,12 +8,32 @@ form.addEventListener('submit', (e) => {
   const { first_name: firstName, last_name: lastName, photo, email } = formData
 
   console.log({ firstName, lastName, photo, email })
-  if (photo) {
+  if (photo.name) {
+    // reset last child of body
+    document.body.lastChild.remove()
+    // Creando el elemento section para la foto
     const reader = new window.FileReader()
     reader.readAsDataURL(photo)
     reader.onloadend = function () {
-      const newPhoto = document.querySelector('#photo')
+      const info = document.createElement('section')
+      info.setAttribute('id', 'info')
+      document.body.appendChild(info)
+      // Creando el elemento img para la foto
+      const newPhoto = document.createElement('img')
       newPhoto.src = reader.result
+      info.appendChild(newPhoto)
+      // Flex
+      const div = document.createElement('div')
+      div.classList.add('info-container')
+      info.appendChild(div)
+      // Creando el elemento h2 para el nombre
+      const name = document.createElement('h2')
+      name.textContent = `${firstName} ${lastName}`
+      div.appendChild(name)
+      // Creando el elemento p para el email
+      const newEmail = document.createElement('p')
+      newEmail.textContent = email
+      div.appendChild(newEmail)
     }
   }
 })
